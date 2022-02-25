4e981ad6901b3508da98fbba10ffb2e2e0338501
Miguel Fernando Marlés Betancourt <80717402+ferchitoM@users.noreply.github.com>
2022-02-25 10:48:59 -0500

    Update shop

--- js/app.js
+++ js/app.js
@@ -2,14 +2,20 @@ Vue.createApp({
   data() {
     return {
       shop_state: "",
+      img_id: 0,
     };
   },
   methods: {
     openShop() {
       if (this.shop_state == "" || this.shop_state == "shop_close")
-        this.shop_state = "shop_open"
-      else this.shop_state = "shop_close"
+        this.shop_state = "shop_open";
+      else this.shop_state = "shop_close";
       //console.log(this.shop_state);
+    },
+    showImage(i) {
+      i % 4 == 0 ? (this.img_id = 1) : this.img_id++;
+      console.log(this.img_id);
+      return this.img_id;
     },
   },
 }).mount("#app");
